c49772720c342cc6677a68d6d16c9829523d9c3f
andrew.sexton <andrew.sexton@wunderkind.co>
2025-02-25 15:42:50 -0500

    filtering css results, adding useEffect for when those change

--- src/ui/DevtoolsPanel.tsx
+++ src/ui/DevtoolsPanel.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Button, Typography, spacingMap } from "@frontend/wknd-components";
 import { getFixedAndStickySelectors } from "../js/automator";
@@ -15,12 +15,11 @@ interface Rule {
 }
 
 interface Result {
-  noQuery: Rule[];
-  mediaQueries: { [key: string]: Rule[] };
+  [key: string]: Rule[];
 }
 
 function parseCSS(str: string): Result {
-  const result: Result = { noQuery: [], mediaQueries: {} };
+  const result: Result = { noQuery: [] };
   let currentRes = "";
   let currentQuery = "";
   let currentSelectors = "";
@@ -39,39 +38,49 @@ function parseCSS(str: string): Result {
       currentRes = "";
     } else if (char === "}") {
       currentRes = currentRes.trim();
-      if (currentSelectors) {
+      if (!!currentSelectors) {
         currentRule = currentRes;
-        if (!currentQuery) {
-          if (!result.noQuery.some((item) => item.rule === currentRule)) {
-            var resultToPush: Rule = {
-              selectors: currentSelectors.split(",").map((item) => item.trim()),
-              rule: currentRule,
-            };
-            // resultToPush.selectors = currentSelectors.split(",").map((item) => item.trim());
-            // resultToPush.rule = currentRule;
-            result.noQuery.push(resultToPush);
+        var resultToPush: Rule = {
+          selectors: Array.from(
+            new Set(currentSelectors.split(",").map((item) => item.trim())),
+          ),
+          rule: currentRule,
+        };
+        if (!!currentQuery) {
+          if (!result[currentQuery]) {
+            result[currentQuery] = [];
+          }
+          if (result[currentQuery].length > 0) {
+            let found = false;
+            result[currentQuery].forEach((item: Rule) => {
+              if (item.rule === resultToPush.rule) {
+                item.selectors = item.selectors.concat(resultToPush.selectors);
+                found = true;
+              }
+            });
+            if (!found) {
+              result[currentQuery].push(resultToPush);
+            }
           } else {
-            const matchIndex = result.noQuery.findIndex(
-              (item) => item.rule === currentRule,
-            );
-            result.noQuery[matchIndex].selectors = result.noQuery[
-              matchIndex
-            ].selectors.concat(
-              currentSelectors.split(",").map((item) => item.trim()),
-            );
+            result[currentQuery].push(resultToPush);
           }
         } else {
-          if (!result.mediaQueries[currentQuery]) {
-            result.mediaQueries[currentQuery] = [];
+          if (result["noQuery"].length > 0) {
+            let found = false;
+            result["noQuery"].forEach((item: Rule) => {
+              if (item.rule === resultToPush.rule) {
+                item.selectors = item.selectors.concat(resultToPush.selectors);
+                found = true;
+              }
+            });
+            if (!found) {
+              result["noQuery"].push(resultToPush);
+            }
+          } else {
+            result["noQuery"].push(resultToPush);
           }
-          var resultToPush: Rule = {
-            selectors: currentSelectors.split(",").map((item) => item.trim()),
-            rule: currentRule,
-          };
-          // resultToPush.selectors = currentSelectors.split(",").map((item) => item.trim());
-          // resultToPush.rule = currentRule;
-          result.mediaQueries[currentQuery].push(resultToPush);
         }
+
         currentSelectors = "";
         currentRule = "";
         currentRes = "";
@@ -83,6 +92,7 @@ function parseCSS(str: string): Result {
     }
   }
 
+  console.log({ result });
   return result;
 }
 
@@ -153,48 +163,34 @@ function DevtoolsPanel() {
       });
   }
 
-  function organizeStyles(styles: {
-    stickyCSS: string;
-    fixedCSS: string;
-    stickyOverZeroCSS: string;
-  }) {
-    // console.log({ styles });
-    var result = {};
-    var fullString =
-        styles.stickyCSS + styles.fixedCSS + styles.stickyOverZeroCSS,
-      queries = fullString.matchAll(/@media[^{]+/gi);
-    queries.forEach((query) => {
-      // console.log({ query });
-      if (!Object.keys(result).includes(query[0].trim())) {
-        result[query[0].trim()] = [];
+  useEffect(() => {
+    console.log("Styles updated", styles);
+    var rules: string[] = [];
+    Object.keys(styles).forEach((key) => {
+      console.log("Key", key);
+      if (key === "noQuery") {
+        styles[key].forEach((item: Rule) => {
+          item.selectors.forEach((selector) => {
+            rules.push(selector + " { " + item.rule + "; }");
+          });
+        });
+      } else {
+        styles[key].forEach((item: Rule) => {
+          item.selectors.forEach((selector) => {
+            rules.push(key + " {" + selector + " { " + item.rule + "; }}");
+          });
+        });
       }
     });
-    console.log({ result });
-  }
+    console.log("Rules", rules);
+  }, [styles]);
 
   return (
     <div style={{ margin: spacingMap.md }}>
       <Typography mb={spacingMap.md} variant="displayLarge">
-        {extnTitle}!!!
+        {extnTitle}
       </Typography>
 
-      {/* <Button
-        buttonText="Send Message to Devtools"
-        mb={spacingMap.md}
-        onClick={() => {
-          handleMessageRequestClick(postToDevtools, setDevtoolsMessage);
-        }}
-        variant="primary"
-      /> */}
-
-      {/* <Button
-        buttonText="Gather resources"
-        mb={spacingMap.md}
-        onClick={() => {
-          handleMessageRequestClick(gatherResources, setBackgroundMessage);
-        }}
-        variant="primary"
-      /> */}
       <Button
         buttonText={"Get Styles"}
         mb={spacingMap.md}
@@ -206,34 +202,21 @@ function DevtoolsPanel() {
               func: getFixedAndStickySelectors,
             },
             (results: any) => {
-              console.log({ results });
-              // setStyles(results[0].result);
-              // organizeStyles(results[0].result);
               var fullString =
                 results[0].result.stickyCSS +
                 results[0].result.fixedCSS +
                 results[0].result.stickyOverZeroCSS;
-              var parsed = parseCSS(fullString);
-              console.log({ parsed });
+              setStyles(parseCSS(fullString));
             },
           );
         }}
         variant="primary"
       />
 
-      {/* <Button 
-        buttonText="Send Message to Background"
-        mb={spacingMap.md}
-        onClick={() => {
-          handleMessageRequestClick(postToBackground, setBackgroundMessage);
-        }}
-        variant="primary"
-      /> */}
-
       <Typography variant="bodyCopy">{devToolsMessage}</Typography>
       <Typography variant="bodyCopy">{backgroundMessage}</Typography>
 
-      {styles.fixedCSS && (
+      {/* {styles.fixedCSS && (
         <div>
           <h3>For Fixed Elements</h3>
           <pre className="wrap">{styles.fixedCSS}</pre>
@@ -252,7 +235,7 @@ function DevtoolsPanel() {
           <h3>For Elements with Sticky Top over 0</h3>
           <pre className="wrap">{styles.stickyOverZeroCSS}</pre>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
